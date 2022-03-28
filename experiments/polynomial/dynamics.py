61ddc999c659175a13889c33c104c02573e1240b
Frederik Baymler Mathiesen <frederik@baymler.com>
2022-03-28 11:43:28 +0200

    Move geometry overlap functions to utils

--- experiments/polynomial/dynamics.py
+++ experiments/polynomial/dynamics.py
@@ -8,29 +8,7 @@ from torch import nn
 from torch.distributions import Normal
 
 from learned_cbf.dynamics import StochasticDynamics
-
-
-def overlap_rectangle(partition_lower, partition_upper, rect_lower, rect_upper):
-    # Separating axis theorem
-    return (partition_upper[..., 0] >= rect_lower[0]) & (partition_lower[..., 0] <= rect_upper[0]) & \
-           (partition_upper[..., 1] >= rect_lower[1]) & (partition_lower[..., 1] <= rect_upper[1])
-
-
-def overlap_circle(partition_lower, partition_upper, center, radius):
-    closest_point = torch.max(partition_lower, torch.min(partition_upper, center))
-    distance = (closest_point - center).norm(dim=-1)
-    return distance <= radius
-
-
-def overlap_outside_rectangle(partition_lower, partition_upper, rect_lower, rect_upper):
-    return (partition_upper[..., 0] >= rect_upper[0]) | (partition_lower[..., 0] <= rect_lower[0]) | \
-           (partition_upper[..., 1] >= rect_upper[1]) | (partition_lower[..., 1] <= rect_lower[1])
-
-
-def overlap_outside_circle(partition_lower, partition_upper, center, radius):
-    farthest_point = torch.where((partition_lower - center).abs() > (partition_upper - center).abs(), partition_lower, partition_upper)
-    distance = (farthest_point - center).norm(dim=-1)
-    return distance >= radius
+from learned_cbf.utils import overlap_circle, overlap_rectangle, overlap_outside_circle, overlap_outside_rectangle
 
 
 class Polynomial(StochasticDynamics, nn.Module):
--- learned_cbf/utils.py
+++ learned_cbf/utils.py
@@ -1,6 +1,31 @@
 import json
 
+import torch
+
 
 def load_config(config_path):
     with open(config_path, 'r') as f:
         return json.load(f)
+
+
+def overlap_rectangle(partition_lower, partition_upper, rect_lower, rect_upper):
+    # Separating axis theorem
+    return (partition_upper[..., 0] >= rect_lower[0]) & (partition_lower[..., 0] <= rect_upper[0]) & \
+           (partition_upper[..., 1] >= rect_lower[1]) & (partition_lower[..., 1] <= rect_upper[1])
+
+
+def overlap_circle(partition_lower, partition_upper, center, radius):
+    closest_point = torch.max(partition_lower, torch.min(partition_upper, center))
+    distance = (closest_point - center).norm(dim=-1)
+    return distance <= radius
+
+
+def overlap_outside_rectangle(partition_lower, partition_upper, rect_lower, rect_upper):
+    return (partition_upper[..., 0] >= rect_upper[0]) | (partition_lower[..., 0] <= rect_lower[0]) | \
+           (partition_upper[..., 1] >= rect_upper[1]) | (partition_lower[..., 1] <= rect_lower[1])
+
+
+def overlap_outside_circle(partition_lower, partition_upper, center, radius):
+    farthest_point = torch.where((partition_lower - center).abs() > (partition_upper - center).abs(), partition_lower, partition_upper)
+    distance = (farthest_point - center).norm(dim=-1)
+    return distance >= radius
