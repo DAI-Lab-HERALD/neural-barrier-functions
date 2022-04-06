5b2615ddef5d6e76fe6894626f2a6ae7ba03b74c
Frederik Baymler Mathiesen <frederik@baymler.com>
2022-04-06 10:53:45 +0200

    Switch polynomial system to use splitting certifier

--- experiments/polynomial/partitioning.py
+++ experiments/polynomial/partitioning.py
@@ -53,29 +53,36 @@ def polynomial_partitioning(config, dynamics):
 
     assert partitioning_config['method'] == 'grid'
 
-    x1_space = torch.linspace(-3.5, 2.0, partitioning_config['num_slices'][0] + 1)
-    x1_cell_width = (x1_space[1] - x1_space[0]) / 2
-    x1_slice_centers = (x1_space[:-1] + x1_space[1:]) / 2
-
-    x2_space = torch.linspace(-2.0, 1.0, partitioning_config['num_slices'][1] + 1)
-    x2_cell_width = (x2_space[1] - x2_space[0]) / 2
-    x2_slice_centers = (x2_space[:-1] + x2_space[1:]) / 2
-
-    cell_width = torch.stack([x1_cell_width, x2_cell_width], dim=-1)
-
-    cell_centers = torch.cartesian_prod(x1_slice_centers, x2_slice_centers)
-    lower_x, upper_x = cell_centers - cell_width, cell_centers + cell_width
-
-    initial_mask = dynamics.initial(cell_centers, cell_width)
-    safe_mask = dynamics.safe(cell_centers, cell_width)
-    unsafe_mask = dynamics.unsafe(cell_centers, cell_width)
-
     partitioning = Partitioning(
-        (lower_x[initial_mask], upper_x[initial_mask]),
-        (lower_x[safe_mask], upper_x[safe_mask]),
-        (lower_x[unsafe_mask], upper_x[unsafe_mask]),
-        (lower_x, upper_x)
+        (torch.tensor([[-1.8, -0.1], [-1.4, -0.5], [1.0, -0.5]]), torch.tensor([[-1.4, 0.1], [-1.2, 0.1], [2.0, 0.5]])),
+        (torch.tensor([[-3.5, -2]]), torch.tensor([[2.0, 1.0]])),
+        (torch.tensor([[0.4, 0.3], [0.4, 0.1], [-1.4, -1.4]]), torch.tensor([[0.6, 0.5], [0.8, 0.3], [-0.6, -0.6]])),
+        (torch.tensor([[-3.5, -2]]), torch.tensor([[2.0, 1.0]]))
     )
+
+    # x1_space = torch.linspace(-3.5, 2.0, partitioning_config['num_slices'][0] + 1)
+    # x1_cell_width = (x1_space[1] - x1_space[0]) / 2
+    # x1_slice_centers = (x1_space[:-1] + x1_space[1:]) / 2
+    #
+    # x2_space = torch.linspace(-2.0, 1.0, partitioning_config['num_slices'][1] + 1)
+    # x2_cell_width = (x2_space[1] - x2_space[0]) / 2
+    # x2_slice_centers = (x2_space[:-1] + x2_space[1:]) / 2
+    #
+    # cell_width = torch.stack([x1_cell_width, x2_cell_width], dim=-1)
+    #
+    # cell_centers = torch.cartesian_prod(x1_slice_centers, x2_slice_centers)
+    # lower_x, upper_x = cell_centers - cell_width, cell_centers + cell_width
+    #
+    # initial_mask = dynamics.initial(cell_centers, cell_width)
+    # safe_mask = dynamics.safe(cell_centers, cell_width)
+    # unsafe_mask = dynamics.unsafe(cell_centers, cell_width)
+    #
+    # partitioning = Partitioning(
+    #     (lower_x[initial_mask], upper_x[initial_mask]),
+    #     (lower_x[safe_mask], upper_x[safe_mask]),
+    #     (lower_x[unsafe_mask], upper_x[unsafe_mask]),
+    #     (lower_x, upper_x)
+    # )
 
     # plot_partitioning(partitioning)
 
