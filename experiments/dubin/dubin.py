Restructure beta computation as a nn.Module (with Mean and Sub modules) to ensure correct bounds

experiments/dubin/dubin.py
@@ -3,12 +3,12 @@ import logging
 import os.path
 
 import torch
-from bound_propagation import BoundModelFactory
 from torch import optim
 from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data import DataLoader
 from tqdm import trange, tqdm
 
+from bounds import LearnedCBFBoundModelFactory
 from monte_carlo import monte_carlo_simulation
 from .dynamics import DubinsCarUpdate, BoundDubinsCarUpdate, DubinsFixedStrategy, BoundDubinsFixedStrategy, \
     DubinsCarNoActuation, BoundDubinsCarNoActuation, DubinsCarStrategyComposition, DubinsCarNNStrategy
@@ -110,7 +110,7 @@ def dubins_car_main(args, config):
     dynamics = DubinsCarStrategyComposition(config['dynamics'], DubinsCarNoActuation()).to(args.device)
 
     if config['experiment_type'] == 'barrier_function':
-        factory = BoundModelFactory()
+        factory = LearnedCBFBoundModelFactory()
         factory.register(DubinsCarUpdate, BoundDubinsCarUpdate)
         factory.register(DubinsFixedStrategy, BoundDubinsFixedStrategy)
         factory.register(DubinsCarNoActuation, BoundDubinsCarNoActuation)

experiments/polynomial/polynomial.py
@@ -9,6 +9,7 @@ from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data import DataLoader
 from tqdm import trange, tqdm
 
+from bounds import LearnedCBFBoundModelFactory
 from dataset import StochasticSystemDataset
 from monte_carlo import monte_carlo_simulation
 from .dynamics import Polynomial, PolynomialUpdate, BoundPolynomialUpdate
@@ -109,7 +110,7 @@ def polynomial_main(args, config):
     dynamics = Polynomial(config['dynamics']).to(args.device)
 
     if config['experiment_type'] == 'barrier_function':
-        factory = BoundModelFactory()
+        factory = LearnedCBFBoundModelFactory()
         factory.register(PolynomialUpdate, BoundPolynomialUpdate)
         factory.register(ButcherTableau, BoundButcherTableau)
 

experiments/population/population.py
@@ -3,12 +3,12 @@ import logging
 import os.path
 
 import torch
-from bound_propagation import BoundModelFactory
 from torch import optim
 from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data import DataLoader
 from tqdm import trange, tqdm
 
+from bounds import LearnedCBFBoundModelFactory
 from monte_carlo import monte_carlo_simulation
 from .dynamics import Population
 from .partitioning import population_partitioning, plot_partitioning
@@ -108,7 +108,7 @@ def population_main(args, config):
     dynamics = Population(config['dynamics']).to(args.device)
 
     if config['experiment_type'] == 'barrier_function':
-        factory = BoundModelFactory()
+        factory = LearnedCBFBoundModelFactory()
         barrier = FCNNBarrierNetwork(network_config=config['model']).to(args.device)
         partitioning = population_partitioning(config, dynamics).to(args.device)
         learner = AdversarialNeuralSBF(barrier, dynamics, factory, horizon=config['dynamics']['horizon']).to(args.device)

learned_cbf/bounds.py
@@ -1,7 +1,8 @@
 from functools import partial
 
 import torch
-from bound_propagation import HyperRectangle
+from bound_propagation import HyperRectangle, BoundModule, LinearBounds, IntervalBounds, BoundModelFactory
+from torch import nn
 
 
 class Affine:
@@ -144,3 +145,59 @@ def bounds(model, partitions, bound_lower=True, bound_upper=True, method='ibp', 
         raise NotImplementedError()
 
     return method(lower, upper, batch_size, bound_lower=bound_lower, bound_upper=bound_upper, **kwargs)
+
+
+class Mean(nn.Module):
+    def __init__(self, subnetwork):
+        super().__init__()
+
+        self.subnetwork = subnetwork
+
+    def forward(self, x):
+        return self.subnetwork(x).mean(dim=0)
+
+
+class BoundMean(BoundModule):
+    def __init__(self, module, factory, **kwargs):
+        super().__init__(module, factory, **kwargs)
+
+        self.subnetwork = factory.build(module.subnetwork)
+
+    @property
+    def need_relaxation(self):
+        return self.subnetwork.need_relaxation
+
+    def clear_relaxation(self):
+        self.subnetwork.clear_relaxation()
+
+    def backward_relaxation(self, region):
+        return self.subnetwork.backward_relaxation(region)
+
+    def crown_backward(self, linear_bounds):
+        subnetwork_bounds = self.subnetwork.crown_backward(linear_bounds)
+
+        if linear_bounds.lower is None:
+            lower = None
+        else:
+            lower = (subnetwork_bounds.lower[0].mean(dim=0), subnetwork_bounds.lower[1].mean(dim=0))
+
+        if linear_bounds.upper is None:
+            upper = None
+        else:
+            upper = (subnetwork_bounds.upper[0].mean(dim=0), subnetwork_bounds.upper[1].mean(dim=0))
+
+        return LinearBounds(linear_bounds.region, lower, upper)
+
+    def ibp_forward(self, bounds, save_relaxation=False):
+        subnetwork_bounds = self.subnetwork.ibp_forward(bounds, save_relaxation=save_relaxation)
+        return IntervalBounds(bounds.region, subnetwork_bounds.lower.mean(dim=0), subnetwork_bounds.upper.mean(dim=0))
+
+    def propagate_size(self, in_size):
+        return self.subnetwork.propagate_size(in_size)
+
+
+class LearnedCBFBoundModelFactory(BoundModelFactory):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.register(Mean, BoundMean)

learned_cbf/certifier.py
@@ -1,31 +1,13 @@
 import logging
 
 import torch
-from bound_propagation import IntervalBounds, LinearBounds
 from torch import nn
 
-from bounds import bounds
-from learned_cbf.partitioning import Partitions
-
+from .bounds import bounds
+from .partitioning import Partitions
+from .networks import BetaNetwork
 
 logger = logging.getLogger(__name__)
-
-
-def reduce_mean(bounds):
-    if isinstance(bounds, IntervalBounds):
-        return IntervalBounds(
-            bounds.region,
-            bounds.lower.mean(dim=0) if bounds.lower is not None else None,
-            bounds.upper.mean(dim=0) if bounds.upper is not None else None
-        )
-    elif isinstance(bounds, LinearBounds):
-        return LinearBounds(
-            bounds.region,
-            (bounds.lower[0].mean(dim=0), bounds.lower[1].mean(dim=0)) if bounds.lower is not None else None,
-            (bounds.upper[0].mean(dim=0), bounds.upper[1].mean(dim=0)) if bounds.upper is not None else None
-        )
-    else:
-        raise ValueError('Bounds can only be linear or interval')
 
 
 class NeuralSBFCertifier(nn.Module):
@@ -33,7 +15,7 @@ class NeuralSBFCertifier(nn.Module):
         super().__init__()
 
         self.barrier = factory.build(barrier)
-        self.barrier_dynamics = factory.build(nn.Sequential(dynamics, barrier))
+        self.beta_network = factory.build(BetaNetwork(dynamics, barrier))
         self.dynamics = dynamics
 
         # Assumptions:
@@ -56,23 +38,17 @@ class NeuralSBFCertifier(nn.Module):
         if kwargs.get('method') == 'optimal':
             kwargs.pop('method')
 
-            _, upper_ibp = bounds(self.barrier_dynamics, self.partitioning.safe, bound_lower=False, method='ibp', reduce=reduce_mean, **kwargs)
-            _, upper_crown = bounds(self.barrier_dynamics, self.partitioning.safe, bound_lower=False, method='crown_linear', reduce=reduce_mean, **kwargs)
+            _, upper_ibp = bounds(self.beta_network, self.partitioning.safe, bound_lower=False, method='ibp', **kwargs)
+            _, upper_crown = bounds(self.beta_network, self.partitioning.safe, bound_lower=False, method='crown_interval', **kwargs)
 
-            lower_ibp, _ = bounds(self.barrier, self.partitioning.safe, bound_upper=False, method='ibp', **kwargs)
-            lower_crown, _ = bounds(self.barrier, self.partitioning.safe, bound_upper=False, method='crown_linear', **kwargs)
+            beta_ibp = upper_ibp.partition_max().max().clamp(min=0)
+            beta_crown = upper_crown.partition_max().max().clamp(min=0)
 
-            beta_ibp_ibp = (upper_ibp - lower_ibp).partition_max().max().clamp(min=0)
-            beta_ibp_crown = (upper_ibp - lower_crown).partition_max().max().clamp(min=0)
-            beta_crown_ibp = (upper_crown - lower_ibp).partition_max().max().clamp(min=0)
-            beta_crown_crown = (upper_crown - lower_crown).partition_max().max().clamp(min=0)
-
-            beta = torch.min(torch.stack([beta_ibp_ibp, beta_ibp_crown, beta_crown_ibp, beta_crown_crown]))
+            beta = torch.min(beta_ibp, beta_crown)
         else:
-            _, upper = bounds(self.barrier_dynamics, self.partitioning.safe, bound_lower=False, reduce=reduce_mean, **kwargs)
-            lower, _ = bounds(self.barrier, self.partitioning.safe, bound_upper=False, **kwargs)
+            _, upper = bounds(self.beta_network, self.partitioning.safe, bound_lower=False, **kwargs)
 
-            beta = (upper - lower).partition_max().max().clamp(min=0)
+            beta = upper.partition_max().max().clamp(min=0)
 
         return beta
 
@@ -176,7 +152,7 @@ class SplittingNeuralSBFCertifier(nn.Module):
         super().__init__()
 
         self.barrier = factory.build(barrier)
-        self.barrier_dynamics = factory.build(nn.Sequential(dynamics, barrier))
+        self.beta_network = factory.build(BetaNetwork(dynamics, barrier))
         self.dynamics = dynamics
 
         # Assumptions:
@@ -199,10 +175,9 @@ class SplittingNeuralSBFCertifier(nn.Module):
         min, max = self.min_max_beta(set, **kwargs)
         last_gap = [torch.finfo(min.dtype).max for _ in range(10)]
 
-        while not self.should_stop_beta_gamma(set, min, max, last_gap):
+        while not self.should_stop_beta_gamma('BETA', set, min, max, last_gap):
             last_gap.append((max - min).max().item())
             last_gap.pop(0)
-            logger.debug(f'[BETA] Gap: {last_gap[-1]}, set size: {len(set)}, upper bound: {max.max().item()}')
 
             set, prune_all = self.prune_beta_gamma(set, min, max)
 
@@ -221,41 +196,30 @@ class SplittingNeuralSBFCertifier(nn.Module):
         if kwargs.get('method') == 'optimal':
             kwargs.pop('method')
 
-            expectation_lower_ibp, expectation_upper_ibp = bounds(self.barrier_dynamics, set, method='ibp', reduce=reduce_mean, **kwargs)
-            expectation_lower_crown, expectation_upper_crown = bounds(self.barrier_dynamics, set, method='crown_linear', reduce=reduce_mean, **kwargs)
+            lower_ibp, upper_ibp = bounds(self.beta_network, set, method='ibp', **kwargs)
+            lower_crown, upper_crown = bounds(self.beta_network, set, method='crown_interval', **kwargs)
 
-            lower_ibp, upper_ibp = bounds(self.barrier, set, method='ibp', **kwargs)
-            lower_crown, upper_crown = bounds(self.barrier, set, method='crown_linear', **kwargs)
+            beta_ibp = upper_ibp.partition_max()
+            beta_crown = upper_crown.partition_max()
+            max = torch.min(beta_ibp, beta_crown)
 
-            beta_ibp_ibp = (expectation_upper_ibp - lower_ibp).partition_max()
-            beta_ibp_crown = (expectation_upper_ibp - lower_crown).partition_max()
-            beta_crown_ibp = (expectation_upper_crown - lower_ibp).partition_max()
-            beta_crown_crown = (expectation_upper_crown - lower_crown).partition_max()
-            max = torch.stack([beta_ibp_ibp, beta_ibp_crown, beta_crown_ibp, beta_crown_crown]).min(dim=0).values
-
-            beta_ibp_ibp = (expectation_lower_ibp - upper_ibp).partition_min()
-            beta_ibp_crown = (expectation_lower_ibp - upper_crown).partition_min()
-            beta_crown_ibp = (expectation_lower_crown - upper_ibp).partition_min()
-            beta_crown_crown = (expectation_lower_crown - upper_crown).partition_min()
-            min = torch.stack([beta_ibp_ibp, beta_ibp_crown, beta_crown_ibp, beta_crown_crown]).max(dim=0).values
+            beta_ibp = lower_ibp.partition_min()
+            beta_crown = lower_crown.partition_min()
+            min = torch.max(beta_ibp, beta_crown)
         else:
-            expectation_lower, expectation_upper = bounds(self.barrier_dynamics, set, reduce=reduce_mean, **kwargs)
-            lower, upper = bounds(self.barrier, set, **kwargs)
+            lower, upper = bounds(self.beta_network, set, **kwargs)
 
-            max = (expectation_upper - lower).partition_max()
-            min = (expectation_lower - upper).partition_min()
+            max = upper.partition_max()
+            min = lower.partition_min()
 
         return min.view(-1), max.view(-1)
 
     def split_beta(self, set, **kwargs):
         kwargs.pop('method', None)
 
-        expectation_lower, expectation_upper = bounds(self.barrier_dynamics, set, method='crown_linear', reduce=reduce_mean, **kwargs)
-        lower, upper = bounds(self.barrier, set, method='crown_linear', **kwargs)
-        upperA = expectation_upper.A - lower.A
-        lowerA = expectation_lower.A - upper.A
+        lower, upper = bounds(self.beta_network, set, method='crown_linear', **kwargs)
 
-        split_dim = ((upperA.abs() + lowerA.abs())[:, 0] * set.width).argmax(dim=-1)
+        split_dim = ((upper.A.abs() + lower.A.abs())[:, 0] * set.width).argmax(dim=-1)
         partition_indices = torch.arange(0, set.lower.size(0), device=set.lower.device)
         split_dim = (partition_indices, split_dim)
 
@@ -292,10 +256,9 @@ class SplittingNeuralSBFCertifier(nn.Module):
         min, max = self.min_max(set, **kwargs)
         last_gap = [torch.finfo(min.dtype).max for _ in range(10)]
 
-        while not self.should_stop_beta_gamma(set, min, max, last_gap):
+        while not self.should_stop_beta_gamma('GAMMA', set, min, max, last_gap):
             last_gap.append((max - min).max().item())
             last_gap.pop(0)
-            logger.debug(f'[GAMMA] Gap: {last_gap[-1]}, set size: {len(set)}, upper bound: {max.max().item()}')
 
             set, prune_all = self.prune_beta_gamma(set, min, max)
 
@@ -321,9 +284,11 @@ class SplittingNeuralSBFCertifier(nn.Module):
 
         return Partitions((set.lower[keep], set.upper[keep])), False
 
-    def should_stop_beta_gamma(self, set, min, max, last_gap):
+    def should_stop_beta_gamma(self, label, set, min, max, last_gap):
         gap = (max - min).max().item()
         abs_max = max.max().item()
+
+        logger.debug(f'[{label}] Gap: {gap}, set size: {len(set)}, upper bound: {max.max().item()}')
 
         return len(set) > self.max_set_size or \
                abs_max <= 0.0 or \
@@ -362,10 +327,9 @@ class SplittingNeuralSBFCertifier(nn.Module):
         min, max = self.min_max(set, **kwargs)
         last_gap = [torch.finfo(min.dtype).max for _ in range(10)]
 
-        while not self.should_stop_violation(set, min, max, lower_bound, last_gap):
+        while not self.should_stop_violation(label, set, min, max, lower_bound, last_gap):
             last_gap.append((max - min).max().item())
             last_gap.pop(0)
-            logger.debug(f'[{label}] Gap: {last_gap[-1]}, set size: {len(set)}, lower bound: {min.min().item()}')
 
             set, prune_all = self.prune_violation(set, min, max, lower_bound)
 
@@ -437,9 +401,12 @@ class SplittingNeuralSBFCertifier(nn.Module):
 
         return Partitions((lower, upper))
 
-    def should_stop_violation(self, set, min, max, lower_bound, last_gap):
+    def should_stop_violation(self, label, set, min, max, lower_bound, last_gap):
         gap = (max - min).max().item()
         abs_min = min.min().item()
+
+        logger.debug(f'[{label}] Gap: {gap}, set size: {len(set)}, lower bound: {min.min().item()}')
+
         return len(set) > self.max_set_size or \
                abs_min >= lower_bound or \
                gap <= self.split_gap_stop_treshold or \

learned_cbf/learner.py
@@ -2,8 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from bound_propagation import IntervalBounds, LinearBounds
-
+from .networks import BetaNetwork
 from .bounds import bounds
 
 
@@ -12,7 +11,7 @@ class AdversarialNeuralSBF(nn.Module):
         super().__init__()
 
         self.barrier = factory.build(barrier)
-        self.barrier_dynamics = factory.build(nn.Sequential(dynamics, barrier))
+        self.beta_network = factory.build(BetaNetwork(dynamics, barrier))
         self.dynamics = dynamics
 
         self.horizon = horizon
@@ -25,63 +24,25 @@ class AdversarialNeuralSBF(nn.Module):
         """
         assert partitioning.safe is not None
 
-        def reduce_mean(bounds):
-            if isinstance(bounds, IntervalBounds):
-                return IntervalBounds(
-                    bounds.region,
-                    bounds.lower.mean(dim=0) if bounds.lower is not None else None,
-                    bounds.upper.mean(dim=0) if bounds.upper is not None else None
-                )
-            elif isinstance(bounds, LinearBounds):
-                return LinearBounds(
-                    bounds.region,
-                    (bounds.lower[0].mean(dim=0), bounds.lower[1].mean(dim=0)) if bounds.lower is not None else None,
-                    (bounds.upper[0].mean(dim=0), bounds.upper[1].mean(dim=0)) if bounds.upper is not None else None
-                )
-            else:
-                raise ValueError('Bounds can only be linear or interval')
-
         if kwargs.get('method') == 'combined':
-            kwargs.pop('method')
-            # with torch.no_grad():
-            #     _, upper = bounds(self.barrier_dynamics, partitioning.safe, bound_lower=False, method='crown_ibp_interval', reduce=reduce_mean, **kwargs)
-            #     lower, _ = bounds(self.barrier, partitioning.safe, bound_upper=False, method='ibp', **kwargs)
-            #
-            #     expectation_no_beta = (upper.mean(dim=0) - lower).partition_max()
-            #     idx = expectation_no_beta.argmax()
-            #
-            #     beta_max_partition = partitioning.safe[idx]
-            #     beta_max_partition = Partitions((
-            #         beta_max_partition.lower.unsqueeze(0),
-            #         beta_max_partition.upper.unsqueeze(0)
-            #     ))
-            #
-            # _, upper = bounds(self.barrier_dynamics, beta_max_partition, bound_lower=False, method='crown_ibp_interval', reduce=reduce_mean, **kwargs)
-            # lower, _ = bounds(self.barrier, beta_max_partition, bound_upper=False, method='ibp', **kwargs)
+            kwargs['method'] = 'crown_ibp_interval'
 
-            _, upper = bounds(self.barrier_dynamics, partitioning.safe, bound_lower=False, method='crown_ibp_interval', reduce=reduce_mean, **kwargs)
-            lower, _ = bounds(self.barrier, partitioning.safe, bound_upper=False, method='ibp', **kwargs)
-        else:
-            # with torch.no_grad():
-            #     _, upper = bounds(self.barrier_dynamics, partitioning.safe, bound_lower=False, reduce=reduce_mean, **kwargs)
-            #     lower, _ = bounds(self.barrier, partitioning.safe, bound_upper=False, **kwargs)
-            #
-            #     expectation_no_beta = (upper - lower).partition_max()
-            #     idx = expectation_no_beta.argmax()
-            #
-            #     beta_max_partition = partitioning.safe[idx]
-            #     beta_max_partition = Partitions((
-            #         beta_max_partition.lower.unsqueeze(0),
-            #         beta_max_partition.upper.unsqueeze(0)
-            #     ))
-            #
-            # _, upper = bounds(self.barrier_dynamics, beta_max_partition, bound_lower=False, reduce=reduce_mean, **kwargs)
-            # lower, _ = bounds(self.barrier, beta_max_partition, bound_upper=False, **kwargs)
+        # with torch.no_grad():
+        #     _, upper = bounds(self.beta_network, partitioning.safe, bound_lower=False, **kwargs)
+        #
+        #     beta = upper.partition_max()
+        #     idx = beta.argmax()
+        #
+        #     beta_max_partition = partitioning.safe[idx]
+        #     beta_max_partition = Partitions((
+        #         beta_max_partition.lower.unsqueeze(0),
+        #         beta_max_partition.upper.unsqueeze(0)
+        #     ))
+        #
+        # _, upper = bounds(self.beta_network, partitioning.safe, bound_lower=False, **kwargs)
 
-            _, upper = bounds(self.barrier_dynamics, partitioning.safe, bound_lower=False, reduce=reduce_mean, **kwargs)
-            lower, _ = bounds(self.barrier, partitioning.safe, bound_upper=False, **kwargs)
-
-        beta = (upper - lower).partition_max().view(-1)
+        _, upper = bounds(self.beta_network, partitioning.safe, bound_lower=False, **kwargs)
+        beta = upper.partition_max().view(-1)
         T = 0.005
         return torch.dot(F.softmax(beta / T, dim=0), beta.clamp(min=0))
         # return beta.clamp(min=0).max()

learned_cbf/networks.py
@@ -1,5 +1,15 @@
-from bound_propagation import Residual
+from bound_propagation import Residual, Sub
 from torch import nn
+
+from bounds import Mean
+
+
+class BetaNetwork(Sub):
+    def __init__(self, dynamics, barrier):
+        super().__init__(
+            Mean(nn.Sequential(dynamics, barrier)),
+            barrier
+        )
 
 
 class FCNNBarrierNetwork(nn.Sequential):