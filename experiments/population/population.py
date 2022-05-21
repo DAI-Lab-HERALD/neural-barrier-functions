4ce2db40b4945fead5e27a7e36436344d60fa5b1
Frederik Mathiesen <frederik_mathies@hotmail.com>
2022-05-21 17:01:10 +0200

    Switch to CROWN interval during testing

--- experiments/population/population.py
+++ experiments/population/population.py
@@ -52,7 +52,7 @@ def test_method(certifier, method, batch_size, kappa=None):
 @torch.no_grad()
 def test(certifier, status_config, kappa=None):
     # test_method(certifier, method='ibp', batch_size=status_config['ibp_batch_size'], kappa=kappa)
-    test_method(certifier, method='crown_linear', batch_size=status_config['crown_ibp_batch_size'], kappa=kappa)
+    test_method(certifier, method='crown_interval', batch_size=status_config['crown_ibp_batch_size'], kappa=kappa)
     # test_method(certifier, method='optimal', batch_size=status_config['crown_ibp_batch_size'], kappa=kappa)
 
 
